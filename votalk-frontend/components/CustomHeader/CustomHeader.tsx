156e046da5095788cccd566950edf798af07a310
asifanwar1 <asifanwar0705@gmail.com>
2026-02-14 18:36:11 +0500

    custom components

--- votalk-frontend/components/CustomHeader/CustomHeader.tsx
+++ votalk-frontend/components/CustomHeader/CustomHeader.tsx
@@ -1,10 +1,34 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { ArrowLeft } from "lucide-react-native";
+import { createHeaderStyles } from "./Styles";
 
-const CustomHeader = () => {
+interface CustomHeaderProps {
+    title: string;
+    showBack?: boolean;
+    backLabel?: string;
+    colors: { primary: string };
+    onBack?: () => void;
+}
+
+const CustomHeader: React.FC<CustomHeaderProps> = ({
+    title,
+    showBack = true,
+    backLabel = "Back",
+    colors,
+    onBack,
+}) => {
+    const styles = createHeaderStyles(colors);
+
     return (
-        <View>
-            <Text>CustomHeader</Text>
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>{title}</Text>
+            {showBack && (
+                <TouchableOpacity style={styles.backBtn} onPress={onBack}>
+                    <ArrowLeft size={18} color="#fff" />
+                    <Text style={styles.saveBtnText}>{backLabel}</Text>
+                </TouchableOpacity>
+            )}
         </View>
     );
 };
---
+++ votalk-frontend/components/CustomHeader/Styles.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+export const createHeaderStyles = (colors: { primary: string }) =>
+    StyleSheet.create({
+        header: {
+            paddingTop: 10,
+            paddingBottom: 24,
+            paddingHorizontal: 24,
+            backgroundColor: colors.primary,
+            borderBottomLeftRadius: 24,
+            borderBottomRightRadius: 24,
+            height: 180,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+        },
+        headerTitle: {
+            color: "#fff",
+            fontSize: 28,
+            fontWeight: "bold",
+        },
+        backBtn: {
+            backgroundColor: colors.primary,
+            borderRadius: 16,
+            paddingVertical: 4,
+            alignItems: "center",
+            flexDirection: "row",
+            gap: 6,
+        },
+        saveBtnText: {
+            color: "#fff",
+            fontWeight: "bold",
+            fontSize: 16,
+        },
+    });
