fec7ea4bd48f359a08e8ac11ac44c74b046af88b
asifanwar1 <asifanwar0705@gmail.com>
2026-02-20 16:27:11 +0500

    code refectoring

--- votalk-frontend/app/(main)/(tabs)/More.tsx
+++ votalk-frontend/app/(main)/(tabs)/More.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, ScrollView } from "react-native";
-import { CircleEllipsis } from "lucide-react-native";
+import { CircleEllipsis, LogOut } from "lucide-react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { useTheme } from "../../../hooks/useTheme";
 import { useMoreStyles } from "./Styles";
 import { useRouter } from "expo-router";
 import { CustomLink } from "@/components/CustomLink/CustomLink";
 import { MORE_ITEMS } from "@/routes/linkConfig";
+import CustomModal from "@/components/CustomModal/CustomModal";
+import { ROUTE_PATHS } from "@/routes/routes";
 
 const More = () => {
     const { colors } = useTheme();
     const styles = useMoreStyles();
     const router = useRouter();
+    const [logoutModalVisible, setLogoutModalVisible] = useState(false);
+
+    const handleLogout = () => {
+        setLogoutModalVisible(false);
+        router.push(ROUTE_PATHS.AUTH.LOGIN);
+    };
+
     return (
         <View style={styles.container}>
             <ScrollView
@@ -47,8 +56,23 @@ const More = () => {
                             onPress={() => router.push(item.route)}
                         />
                     ))}
+                    <CustomLink
+                        icon={<LogOut size={22} color="#8b5cf6" />}
+                        label="Logout"
+                        onPress={() => setLogoutModalVisible(true)}
+                    />
                 </View>
             </ScrollView>
+            <CustomModal
+                visible={logoutModalVisible}
+                onClose={() => setLogoutModalVisible(false)}
+                title="Confirm Logout"
+                confirmText="Logout"
+                cancelText="Cancel"
+                onConfirm={handleLogout}
+            >
+                <Text>Are you sure you want to logout?</Text>
+            </CustomModal>
         </View>
     );
 };
